8cf484886f6dd9d0c716cda8bcb77860d64e8994
Christian De Caro <cdecaro@medphysics>
2022-10-19 16:22:40 -0500

    initial

---
+++ CdAttenuation.m
@@ -0,0 +1,13 @@
+function ratio = CdAttenuation(material, path_length, energy)
+% get the percentage of photons that are attenuated in an object
+% 
+% ratio: percent of photons attenuated (1 - (N/N0))
+% material: ['CdTe', 'CsI', 'Cu', etc.]
+% path_length: path length of beam through material [cm]
+% energy: photon energy (keV)
+
+
+ratio = exp(- CdGetLinearAttenuation(material,energy) * path_length);
+
+
+end
