946a1c5b3014006d30937bb325e56c307247b61b
Christian De Caro <cdecaro@medphysics>
2022-11-07 16:15:39 -0600

    prepopulate row and column numbers

--- FPD/CdReadFpdSeq.m
+++ FPD/CdReadFpdSeq.m
@@ -1,9 +1,7 @@
-function image = CdReadFpdSeq(filename, rows, cols, frames)
+function image = CdReadFpdSeq(filename, frames)
 % read in the .seq file produced by the video functionality of the FPD
 %
 % filename: the name of the file
-% rows: number of rows (image height)
-% cols: number of columns (image width)
 % frames: number of image frames (pages)
 
-image = MgReadRawFile(filename, rows, cols, frames, 2048, 0, 'uint16');
+image = MgReadRawFile(filename, 1536, 2048, frames, 2048, 0, 'uint16');
