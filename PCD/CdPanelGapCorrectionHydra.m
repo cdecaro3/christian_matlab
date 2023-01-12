da94a71081460456a281597b7448e6baa57b3b25
WIMR Cube PC <36656491+cdecaro3@users.noreply.github.com>
2023-01-12 11:48:18 -0600

    new

---
+++ PCD/CdPanelGapCorrectionHydra.m
@@ -0,0 +1,19 @@
+function corrected_img = CdPanelGapCorrectionHydra(img)
+% corrected_img = CdPanelGapCorrectionHydra(img)
+% 
+% interpolate over the gaps between panels for the Hydra PCD
+% 
+% corrected_img: image(s) after correction
+% img: uncorrected image(s) (64 x 5120 x _)
+
+bad_left = 256:256:4864;
+bad_right = 257:256:4865;
+
+corrected_img = img;
+
+corrected_img(:,bad_left,:) = 2/3.*img(:,bad_left-1,:) + 1/3.*img(:,bad_left+2,:);
+corrected_img(:,bad_right,:) = 2/3.*img(:,bad_right+1,:) + 1/3.*img(:,bad_right-2,:);
+
+corrected_img = corrected_img(2:63,2:5119,:);
+
+end
